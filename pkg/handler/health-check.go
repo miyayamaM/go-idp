6155b77173675ffa3c5f37268fcb5da4e4a6c53f
miyayamaM <pendulum.ngt.4010@gmail.com>
2026-02-07 19:22:38 +0900

    Remove test migration file and clean up health-check documentation

--- pkg/handler/health-check.go
+++ pkg/handler/health-check.go
@@ -6,14 +6,6 @@ import (
 	"github.com/labstack/echo/v5"
 )
 
-// handler godoc
-// @Summary users
-// @Description get the users list
-// @Accept  json
-// @Produce  json
-// @Param  service query string true "Service you want to get the users list"
-// @Success 200 {object} map[string]string
-// @Router /health-check [get]
 func HealthCheck(c *echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
 		"status": "ok",
